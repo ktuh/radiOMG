b50904429b48babd27d93810291110901ad2d200
dchan3 <dchan3@hawaii.edu>
2016-12-24 17:37:01 -1000

    Deleted 'shindig' stuff

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -14,7 +14,6 @@ import '../../ui/components/news/news_item.js';
 import '../../ui/components/news/news_list.js';
 import '../../ui/components/chats/chat.js';
 import '../../ui/components/application/not_found.js';
-import '../../ui/components/shindig/shindig.js';
 import '../../ui/components/profile/profile_edit.js';
 import '../../ui/components/profile/profile_page.js';
 import '../../ui/components/shows/show_page.js';
--- imports/ui/components/shindig/shindig.js
+++
@@ -1,6 +0,0 @@
-import './shindig.html';
-import { Template } from 'meteor/templating';
-
-Template.shindig.onRendered(function () {
-  Session.set('documentTitle', 'davey shindig');
-});
