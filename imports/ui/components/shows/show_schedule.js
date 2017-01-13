da164e3e50ec65b4e63d7c102f907d89f846cbd9
dchan3 <dchan3@hawaii.edu>
2017-01-12 17:40:00 -1000

    Schedule template

--- imports/ui/components/shows/show_schedule.js
+++ imports/ui/components/shows/show_schedule.js
@@ -15,29 +15,18 @@ Template.showSchedule.onRendered(function() {
 });
 
 Template.showSchedule.helpers({
-  calendar: function() {
-		var ret = $("<table>", {class: "schedule", border: "1"});
-		var header = $("<tr>", {id: "schedule-header"});
-		var body = $("<tr>", {id: "schedule-body"});
-		var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-		for (var d = 0; d < days.length; d++) {
-			console.log(days[d]);
-			header.append("<td><h1>"+ days[d] + "</h1></td>");
-			var td = $("<td>", {class: "bodyCell"});
-			var list = $("<ul>", {class: "showList"});
-			var res = Shows.find({startDay: days[d], active: true}, {sort: {startHour: 1, startMinute: 1}});
-			res.forEach(function(show) {
-				console.log(show);
-				list.append("<li>" + show.startHour + ":" + (show.startMinute < 10 ? "0" + show.startMinute : show.startMinute) + "-" + show.endHour + ":" + (show.endMinute < 10 ? "0" + show.endMinute : show.endMinute) +
-										": " + show.showName + " with " + show.host + "</li>");
-			});
-			console.log(list.prop("outerHTML"));
-			td.append(list);
-			body.append(td);
-		}
-		ret.append(header);
-		ret.append(body);
-		console.log(ret.prop("outerHTML"));
-		return ret.prop('outerHTML');
+	dayOfTheWeek: function() {
+			var daze = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+			return daze[new Date().getDay()];
+	},
+  showList: function (day) {
+		var list = $("<ul>", {class: "showList"});
+		var res = Shows.find({startDay: day, active: true}, {sort: {startHour: 1, startMinute: 1}});
+		res.forEach(function(show) {
+			console.log(show);
+			list.append("<li>" + show.startHour + ":" + (show.startMinute < 10 ? "0" + show.startMinute : show.startMinute) + "-" + show.endHour + ":" + (show.endMinute < 10 ? "0" + show.endMinute : show.endMinute) +
+									": " + show.showName + " with " + show.host + "</li>");
+		});
+		return list.prop("outerHTML");
 	}
 });
