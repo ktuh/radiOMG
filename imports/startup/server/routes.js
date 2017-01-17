502abd472560c3947a1672b93b5c9318b84946e7
dchan3 <dchan3@hawaii.edu>
2017-01-16 23:37:26 -1000

    Further update to route function

--- imports/startup/server/routes.js
+++ imports/startup/server/routes.js
@@ -1,6 +1,8 @@
 import { Picker } from 'meteor/meteorhacks:picker';
 import { RSS } from 'meteor/rss';
 import { check } from 'meteor/check';
+import { Shows } from '../../api/shows/shows_collection.js';
+import { Playlists } from '../../api/playlists/playlists_collection.js';
 
 Picker.route('/feed.xml', function(params, req, res, next) {
 	var feed = new RSS({
@@ -13,4 +15,11 @@ Picker.route('/feed.xml', function(params, req, res, next) {
 
 Picker.route('/spinitron/latest', function(params, req, res, next) {
 	check(params, {playlistId: Match.Integer, showName: String});
+	var showId = Shows.findOne({active: true, showName: params.showName})._id;
+	var playlist = Playlists.findOne({showId: showId, spinPlaylistId: playlistId});
+	var today = new Date();
+	var newToday = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0, 0);
+  if (!playlist) {
+		Playlists.insert({spinPlaylistId: playlistId, showId: showId, showDate: newToday});
+	}
 });
