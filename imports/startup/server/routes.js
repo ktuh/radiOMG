b296fc8c41ae015f85ed05a4e0e4a770a9331667
dchan3 <dchan3@hawaii.edu>
2017-01-16 23:10:53 -1000

    Started on playlist optimization

--- imports/startup/server/routes.js
+++ imports/startup/server/routes.js
@@ -1,5 +1,7 @@
 import { Picker } from 'meteor/meteorhacks:picker';
 import { RSS } from 'meteor/rss';
+import { check } from 'meteor/check';
+
 Picker.route('/feed.xml', function(params, req, res, next) {
 	var feed = new RSS({
 		title: 'New 808 Mixes',
@@ -7,4 +9,8 @@ Picker.route('/feed.xml', function(params, req, res, next) {
 	});
 	res.write(feed.xml());
 	res.end();
+});
+
+Picker.route('/spinitron/latest', function(params, req, res, next) {
+	check(params, {playlistId: Match.Integer, showName: String});
 });
