e1d135d28d7c9d7ebb5256d0236b24bf84bb96a5
daveywilkie <davey.wilkie@gmail.com>
2017-02-01 13:04:46 -1000

    Mediaplayer wip

--- imports/ui/components/includes/header.js
+++ imports/ui/components/includes/header.js
@@ -20,70 +20,70 @@ import 'mediaelement/player';
 // });
 
 Template.header.onRendered(function () {
-  // var $searchInput = $('.nav__search input');
-  // var mp3 = $('#audio-player').attr('src');
+  var $searchInput = $('.nav__search input');
+  var mp3 = $('#audio-player').attr('src');
 
-  // Session.set('nowLoaded', mp3);
-  // Session.set('nowPlaying', 'Live audio stream.');
-  // $searchInput.css('font-family', 'Glyphicons Halflings');
-  // $searchInput.attr('placeholder', '\ue003');
-  // $searchInput.focusin(function () {
-  //   if (FlowRouter.getRouteName() != 'mix')
-  //     FlowRouter.go('mix');
-  //   $searchInput.css('font-family', 'Sweden Sans');
-  //   $searchInput.attr('placeholder', 'Search DJ, genre, etc.');
-  //   $('.tags').removeClass('hidden');
-  // });
-  // $searchInput.focusout(function() {
-  //   if ($searchInput.val().length === 0) {
-  //     $searchInput.css('font-family', 'Glyphicons Halflings');
-  //     $searchInput.attr('placeholder', '\ue003');
-  //   }
-  //   if (!Session.get('mouseIsOverTag')) {
-  //     $('.tags').addClass('hidden');
-  //   }
-  // });
+  Session.set('nowLoaded', mp3);
+  Session.set('nowPlaying', 'Live audio stream.');
+  $searchInput.css('font-family', 'Glyphicons Halflings');
+  $searchInput.attr('placeholder', '\ue003');
+  $searchInput.focusin(function () {
+    if (FlowRouter.getRouteName() != 'mix')
+      FlowRouter.go('mix');
+    $searchInput.css('font-family', 'Sweden Sans');
+    $searchInput.attr('placeholder', 'Search DJ, genre, etc.');
+    $('.tags').removeClass('hidden');
+  });
+  $searchInput.focusout(function() {
+    if ($searchInput.val().length === 0) {
+      $searchInput.css('font-family', 'Glyphicons Halflings');
+      $searchInput.attr('placeholder', '\ue003');
+    }
+    if (!Session.get('mouseIsOverTag')) {
+      $('.tags').addClass('hidden');
+    }
+  });
 
   // This kludge is necessary because the onRendered callback fires before
   // some of the DOM is fully rendered. Sad.
-  // setTimeout(function() {  
-    $('audio').mediaelementplayer({
-      pluginPath: "/mejs/"
-      // alwaysShowControls: true,
-      // features: ['playpause', 'progress'],
-      // type: 'audio/mp3',
-      // src: 'http://stream.ktuh.org:8000/stream-mp3',
-      // audioWidth: 200,
-      // audioHeight: 20,
-      // iPadUseNativeControls: false,
-      // iPhoneUseNativeControls: false,
-      // AndroidUseNativeControls: false,
-      // success: function (mediaElement, domObject) {
-      //   setSrc(orion.dictionary.get('mainPage.audioUrl'));
-      //   mediaElement.addEventListener('play', function(e) {
-      //     Session.set('paused', false);
-      //   }, false);
-      //   mediaElement.addEventListener('pause', function(e) {
-      //     Session.set('paused', true);
-      //   }, false);
-      //   // Display what's playing if user clicks the player without loading
-      //   // another song first.
-      //   $('.mejs__playpause-button').click(function () {
-      //     if (Session.equals('defaultLoaded', true)) {
-      //       var message = 'Now playing the ' +
-      //           orion.dictionary.get('mainPage.title', 'station\'s') + ' live stream';
-      //       Session.set('defaultLoaded', false);
-      //       Session.set('nowLoaded', orion.dictionary.get('mainPage.audioUrl', ''));
-      //       Bert.alert(message, 'default', 'growl-top-right', 'fa-music');
-      //     }
-      //   });
-      //   player = mediaElement; // make it available for other functions
-      // },
-      // error: function () {
-      //   console.error("Encountered an error while initializing the media element.")
-      // }
+  setTimeout(function() {  
+    $('#audio-player').mediaelementplayer({
+      pluginPath: "/mejs/",
+      alwaysShowControls: true,
+      features: ['playpause', 'progress'],
+      type: 'audio/mp3',
+      src: 'http://stream.ktuh.org:8000/stream-mp3',
+      audioWidth: 200,
+      audioHeight: 20,
+      iPadUseNativeControls: false,
+      iPhoneUseNativeControls: false,
+      AndroidUseNativeControls: false,
+      success: function (mediaElement, domObject) {
+        setSrc(orion.dictionary.get('mainPage.audioUrl'));
+        mediaElement.addEventListener('play', function(e) {
+          Session.set('paused', false);
+        }, false);
+        mediaElement.addEventListener('pause', function(e) {
+          Session.set('paused', true);
+        }, false);
+        // Display what's playing if user clicks the player without loading
+        // another song first.
+        $('.mejs__playpause-button').click(function () {
+          if (Session.equals('defaultLoaded', true)) {
+            var message = 'Now playing the ' +
+                orion.dictionary.get('mainPage.title', 'station\'s') + ' live stream';
+            Session.set('defaultLoaded', false);
+            Session.set('nowLoaded', orion.dictionary.get('mainPage.audioUrl', ''));
+            Bert.alert(message, 'default', 'growl-top-right', 'fa-music');
+          }
+        });
+        player = mediaElement; // make it available for other functions
+      },
+      error: function () {
+        console.error("Encountered an error while initializing the media element.")
+      }
     });
-  // }, 1000);
+  }, 1000);
 });
 
 Template.header.helpers({
