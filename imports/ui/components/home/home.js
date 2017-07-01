6fcd5ca0dc0fccbf3673ed044a99e5124bb446b0
daveywilkie <davey.wilkie@gmail.com>
2017-06-30 15:14:01 -1000

    Updates footer and landing

--- imports/ui/components/home/home.js
+++ imports/ui/components/home/home.js
@@ -2,7 +2,6 @@ import './home.html';
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import { Playlists } from '../../../api/playlists/playlists_collection.js';
 import { Posts } from '../../../api/posts/posts_collection.js';
 import { Reviews } from '../../../api/reviews/reviews_collection.js';
 import { $ } from 'meteor/jquery';
@@ -12,7 +11,6 @@ Template.home.onCreated(function () {
   self.autorun(function () {
     self.subscribe('postsLimited', { limit: 5, sort: { submitted: -1 }});
     self.subscribe('reviews', { limit: 5, sort: { submitted: -1 }});
-    self.subscribe('playlistsLimited', { limit: 10, sort: { showDate: -1 }});
   });
 });
 
@@ -52,11 +50,12 @@ Template.home.onRendered(function() {
   /* Function that manages the direction of the scroll. */
   function offSetManager() {
     var offset = window.pageYOffset + $('.navbar').height();
+    var height = $('.landing').height();
 
-    if (window.innerHeight < offset) {
+    if (height < offset) {
         myNavBar.remove();
     }
-    else if (window.innerHeight >= offset){
+    else if (height >= offset){
         myNavBar.add();
     }
   }
@@ -72,6 +71,10 @@ Template.home.onRendered(function() {
 });
 
 Template.home.onDestroyed(function() {
+  var objs = [$('.navbar'), $('.navbar-default'), $('.dropdown-menu')]
+  for (var i = 0; i < objs.length; i++) {
+    objs[i].removeClass('fixed-theme');
+  }
   window.onscroll = null;
 });
 
@@ -81,12 +84,6 @@ Template.home.helpers({
   },
   posts: () => {
     return Posts.find({}, { sort: { submitted: -1 }});
-  },
-  hasPlaylists: () => {
-    return Playlists.find({}, { sort: { showDate: -1 }}).count() > 0;
-  },
-  playlists: () => {
-    return Playlists.find({}, { sort: { showDate: -1 }});
   },
   hasReviews: () => {
     return Reviews.find({}, { sort: { submitted: -1 }}).count() > 0;
