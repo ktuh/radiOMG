4effb1bbc4937888f308abfbb08d91a8b70b4a57
dchan3 <dchan3@hawaii.edu>
2017-07-09 05:30:31 -1000

    Modify moderator user type permissions

--- imports/startup/lib/moderator.js
+++ imports/startup/lib/moderator.js
@@ -1,3 +1,45 @@
 import { Roles } from 'meteor/nicolaslopezj:roles';
 
 const Moderator = new Roles.Role('moderator');
+
+Moderator.allow('collections.reviews.index', true);
+Moderator.allow('collections.reviews.update', function (userId, doc, fields, modifier) {
+  return doc.userId === userId;
+});
+Moderator.allow('collections.reviews.remove', function (userId, doc, fields, modifier) {
+  return doc.userId === userId;
+});
+Moderator.allow('collections.reviews.showUpdate', function (userId, doc, fields, modifier) {
+  return doc.userId === userId;
+});
+Moderator.allow('collections.reviews.showRemove', function (userId, doc, fields, modifier) {
+  return doc.userId === userId;
+});
+
+Moderator.allow('collections.parties.index', true);
+Moderator.allow('collections.parties.update', function (userId, doc, fields, modifier) {
+  return doc.userId === userId;
+});
+Moderator.allow('collections.parties.remove', function (userId, doc, fields, modifier) {
+  return doc.userId === userId;
+});
+Moderator.allow('collections.parties.showUpdate', function (userId, doc, fields, modifier) {
+  return doc.userId === userId;
+});
+Moderator.allow('collections.parties.showRemove', function (userId, doc, fields, modifier) {
+  return doc.userId === userId;
+});
+
+Moderator.allow('collections.posts.index', true);
+Moderator.allow('collections.posts.update', function (userId, doc, fields, modifier) {
+  return doc.userId === userId;
+});
+Moderator.allow('collections.posts.remove', function (userId, doc, fields, modifier) {
+  return doc.userId === userId;
+});
+Moderator.allow('collections.posts.showUpdate', function (userId, doc, fields, modifier) {
+  return doc.userId === userId;
+});
+Moderator.allow('collections.posts.showRemove', function (userId, doc, fields, modifier) {
+  return doc.userId === userId;
+});
