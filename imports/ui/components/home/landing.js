9ade40a8b72af96af15915833c382885c0d9f612
daveywilkie <davey.wilkie@gmail.com>
2017-10-15 12:08:10 -1000

    Landing hotfix

--- imports/ui/components/home/landing.js
+++ imports/ui/components/home/landing.js
@@ -73,7 +73,7 @@ Template.landing.helpers({
 
 Template.landing.events({
   'click .landing__play-btn-outer': function(event) {
-    var url = 'http://stream.ktuh.org:8000/stream-mp3';
+    var url = scorpius.dictionary.get('mainPage.audioUrl', '');
 
     if (Session.get('paused') === false)
       player.pause();
