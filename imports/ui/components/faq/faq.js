5654135dc46c58622fc9756aed770f1c4a073905
dchan3 <dchan3@hawaii.edu>
2017-10-19 17:31:20 -1000

    FAQ styling

--- imports/ui/components/faq/faq.js
+++ imports/ui/components/faq/faq.js
@@ -1,1 +1,8 @@
 import './faq.html';
+
+Template.faq.events({
+  'click .toggle': function(e) {
+    $(e.target).html($(e.target).html() == '[ + ]' ? '[ - ]' : '[ + ]');
+    $(e.target).parent().siblings('.faq__answer').toggle(100);
+  }
+});
