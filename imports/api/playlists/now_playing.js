2ca6aa78431939d59360c0cd50d5d2dbd2944c7a
dchan3 <dchan3@hawaii.edu>
2017-10-25 15:14:27 -1000

    Add Now Playing collection hooks

--- imports/api/playlists/now_playing.js
+++ imports/api/playlists/now_playing.js
@@ -3,3 +3,6 @@ import { Mongo } from 'meteor/mongo';
 export default NowPlaying = new Mongo.Collection('nowPlaying');
 
 NowPlaying.allow({'insert': function() { return true; }, 'update': function() { return true; }});
+
+NowPlaying.before.insert(() => Session.set('timeout', false));
+NowPlaying.before.update(() => Session.set('timeout', false));
