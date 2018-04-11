3423447d8e0eebdd8705452c8b52afae295ac88f
dchan3 <dchan3@hawaii.edu>
2018-04-10 23:21:20 -1000

    Update moderator permissions + parties post type
    p to div in party description

--- imports/startup/lib/moderator.js
+++ imports/startup/lib/moderator.js
@@ -3,23 +3,29 @@ import { Roles } from 'meteor/nicolaslopezj:roles';
 const Moderator = new Roles.Role('moderator');
 
 Moderator.allow('collections.reviews.index', true);
+Moderator.allow('collections.review.insert', true);
 Moderator.allow('collections.reviews.update', (userId, doc, fields, modifier) =>
   doc.userId === userId);
 Moderator.allow('collections.reviews.remove', true);
+Moderator.allow('collections.reviews.showCreate', true);
 Moderator.allow('collections.reviews.showUpdate', true);
 Moderator.allow('collections.reviews.showRemove', true);
 Moderator.helper('collections.reviews.indexFilter', () => ({ }));
 
 Moderator.allow('collections.parties.index', true);
+Moderator.allow('collections.parties.insert', true);
 Moderator.allow('collections.parties.update', true);
 Moderator.allow('collections.parties.remove', true);
+Moderator.allow('collections.parties.showCreate', true);
 Moderator.allow('collections.parties.showUpdate', true);
 Moderator.allow('collections.parties.showRemove', true);
 Moderator.helper('collections.parties.indexFilter', () => ({ }));
 
 Moderator.allow('collections.posts.index', true);
+Moderator.allow('collections.posts.insert', true);
 Moderator.allow('collections.posts.update', true);
 Moderator.allow('collections.posts.remove', true);
+Moderator.allow('collections.posts.showCreate', true);
 Moderator.allow('collections.posts.showUpdate', true);
 Moderator.allow('collections.posts.showRemove', true);
 Moderator.helper('collections.posts.indexFilter', () => ({ }));
