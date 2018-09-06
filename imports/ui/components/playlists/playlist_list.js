a27dd1145352904e63299171549261b7c006b5a0
dchan3 <dchan3@hawaii.edu>
2018-09-06 08:45:31 -1000

    Refactor for Spinitron upgrade

--- imports/ui/components/playlists/playlist_list.js
+++ imports/ui/components/playlists/playlist_list.js
@@ -5,7 +5,8 @@ import Playlists from '../../../api/playlists/playlists_collection.js';
 import Profiles from '../../../api/users/profiles_collection.js';
 import Shows from '../../../api/shows/shows_collection.js';
 import { pagination } from 'meteor/kurounin:pagination';
-import { moment } from 'meteor/momentjs:moment';
+import { moment as momentUtil } from 'meteor/momentjs:moment';
+import moment from 'moment-timezone';
 import { currentPlaylistFindOne } from '../../../startup/lib/helpers.js';
 
 Template.playlistList.onCreated(function() {
@@ -40,13 +41,25 @@ Template.playlistList.onCreated(function() {
 
               var parsedId = parseInt(playlist.spinPlaylistId);
 
-              Meteor.call('getPlaylistOrInfo', parsedId, true,
-                function(error, result) {
-                  if (!error && result) {
-                    Session.set('currentPlaylist', result);
-                    Session.set('playlistViewing', parsedId)
-                  }
-                });
+              if (parsedId > 10000) {
+                Meteor.call('getPlaylistOrInfo2', parsedId,
+                  function(error, result) {
+                    if (!error && result) {
+                      Session.set('currentPlaylist', result.data.items);
+                      Session.set('playlistViewing', parsedId);
+                    }
+                  });
+              }
+              else {
+                Meteor.call('getPlaylistOrInfo', parsedId, true,
+                  function(error, result) {
+                    if (!error && result) {
+                      Session.set('currentPlaylist',
+                        JSON.parse(result.content).results);
+                      Session.set('playlistViewing', parsedId);
+                    }
+                  });
+              }
             }
           });
       }
@@ -55,14 +68,30 @@ Template.playlistList.onCreated(function() {
 });
 
 Template.playlistList.helpers({
-  latestSongs: () => Session.get('currentPlaylist'),
+  latestSongs: () => {
+    var retval = Session.get('currentPlaylist');
+    retval.sort(function(a,b) {
+      if (a.start > b.start) {
+        return 1;
+      }
+      else if (a.start < b.start) {
+        return -1;
+      }
+      else return 0;
+    });
+    return retval;
+  },
   showById: (id) => Shows.findOne({ showId: id }),
   showTime: (startDay, startHour) => {
     var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
       'Friday', 'Saturday'];
-    return days[startDay] + 's at ' + moment().hour(startHour).format('h A');
+    return days[startDay] + 's at ' +
+      momentUtil().hour(startHour).format('h A');
   },
-  date: (showDate) => moment(showDate).format('dddd, h:mm a,<br>MMM DD YYYY'),
+  date: (showDate) =>
+    momentUtil(
+      moment(showDate, 'Pacific/Honolulu')
+    ).format('dddd, h:mm a,<br>MMM DD YYYY'),
   ready: () => Template.instance().pagination.ready(),
   docs: () => Template.instance().pagination.getPage(),
   tempPag: () => Template.instance().pagination,
@@ -81,12 +110,21 @@ Template.playlistList.helpers({
   latestShow: () => Shows.findOne({ showId: Playlists.findOne({}, {
     sort: { showDate: -1 } }).showId
   }),
-  truncated: (str) => str.substring(0, str.length - 3),
-  timeBeautify: (time) => moment(time, 'HH:mm').format('hh:mma'),
+  truncated: (str) =>
+    /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{4}/.exec(str) === null ?
+      str.substring(0, str.length - 3) : str,
+  timeBeautify: (time) => {
+    return momentUtil(
+      moment(momentUtil(time), 'Pacific/Honolulu')
+    ).format('hh:mma');
+  },
   timeHMS: (date, startTime, endTime) => {
-    return moment(date).format('ddd. MMM DD, YYYY') + ' ' +
-    moment(startTime, 'HH:mm:ss').format('hh:mm') +  '-' +
-    moment(endTime, 'HH:mm:ss').format('hh:mm A'); },
+    return momentUtil(moment(date, 'Pacific/Honolulu'))
+      .format('ddd. MMM DD, YYYY') + ' ' +
+    momentUtil(startTime, 'HH:mm:ss')
+      .format('hh:mm') +  '-' +
+    momentUtil(endTime, 'HH:mm:ss')
+      .format('hh:mm A'); },
   isPlaylistCurrent: () => {
     var current = currentPlaylistFindOne();
     return current !== undefined;
--- imports/ui/components/playlists/playlist_page.js
+++ imports/ui/components/playlists/playlist_page.js
@@ -8,7 +8,8 @@ import Playlists from '../../../api/playlists/playlists_collection.js';
 import Shows from '../../../api/shows/shows_collection.js';
 import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import { moment } from 'meteor/momentjs:moment';
+import { moment as momentUtil } from 'meteor/momentjs:moment';
+import moment from 'moment-timezone';
 
 Template.playlistPage.onCreated(function(){
   var self = this;
@@ -21,15 +22,27 @@ Template.playlistPage.onCreated(function(){
     self.subscribe('playlist', id, {
       onReady: function() {
         var playlist = Playlists.findOne({ spinPlaylistId: id });
-        var parsedId = parseInt(playlist.spinPlaylistId);
 
-        Meteor.call('getPlaylistOrInfo', parsedId, true,
-          function(error, result) {
-            if (!error && result) {
-              Session.set('currentPlaylist', result);
-              Session.set('playlistViewing', parsedId);
-            }
-          });
+        if (id > 10000) {
+          Meteor.call('getPlaylistOrInfo2', id,
+            function(error, result) {
+              if (!error && result) {
+                Session.set('currentPlaylist', result.data.items);
+                Session.set('playlistViewing', id);
+              }
+            });
+        }
+        else {
+          Meteor.call('getPlaylistOrInfo', id, true,
+            function(error, result) {
+              if (!error && result) {
+                Session.set('currentPlaylist',
+                  JSON.parse(result.content).results);
+                Session.set('playlistViewing', id);
+              }
+            });
+        }
+
         self.subscribe('comments', playlist._id);
         if (playlist.showId > -1)
           self.subscribe('showBySpinitronId', playlist.showId);
@@ -48,12 +61,39 @@ Template.playlistPage.helpers({
     spinPlaylistId: parseInt(FlowRouter.getParam('id'))
   }),
   comments: (id) => Comments.find({ postId: id }),
-  songs: () => Session.get('currentPlaylist'),
+  songs: () => {
+    var retval = Session.get('currentPlaylist');
+    retval.sort(function(a,b) {
+      if (a.start > b.start) {
+        return 1;
+      }
+      else if (a.start < b.start) {
+        return -1;
+      }
+      else return 0;
+    });
+    return retval;
+  },
   showTime: (playlist) =>
-    playlist && (moment(playlist.startTime, 'HH:mm:ss').format('h:mm') +
-      '-' + moment(playlist.endTime, 'HH:mm:ss').format('h:mm a')),
+    playlist && ((momentUtil(
+      moment(
+        momentUtil(playlist.startTime, 'HH:mm:ss')).tz('Pacific/Honolulu'))
+    ).format('h:mm') + '-' +
+      momentUtil(
+        moment(momentUtil(playlist.endTime, 'HH:mm:ss'))
+          .tz('Pacific/Honolulu')).format('h:mm a')),
   showDateOfLatestPlaylist: (date) =>
-    moment(date).utcOffset('-10:00').format('LL'),
-  timeBeautify: (time) =>
-    moment(time.substring(0, time.length - 3), 'HH:mm').format('hh:mm a')
+    momentUtil(moment(date).tz('Pacific/Honolulu')).format('LL'),
+  timeBeautify: (time) => {
+    if (/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{4}/.exec(time) === null) {
+      return momentUtil(
+        time.substring(0, time.length - 3), 'HH:mm'
+      ).format('hh:mma');
+    }
+    else {
+      return momentUtil(
+        moment(momentUtil(time), 'Pacific/Honolulu')
+      ).format('hh:mma');
+    }
+  }
 });
