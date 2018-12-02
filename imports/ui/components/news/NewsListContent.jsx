d2b7662acedf38afc9e38e92fd1ebc68c98191ae
dchan3 <dchan3@hawaii.edu>
2018-12-02 11:17:39 -1000

    Squash bugs

--- imports/ui/components/news/NewsListContent.jsx
+++ imports/ui/components/news/NewsListContent.jsx
@@ -56,13 +56,14 @@ class NewsListContent extends Component {
 export default withTracker(() => {
   var s1 = Meteor.subscribe('posts'),
     s2 = Meteor.subscribe('latestFeaturedPost'),
-    s3 = Meteor.subscribe('djProfiles');
+    s3 = Meteor.subscribe('djs'),
+    s4 = Meteor.subscribe('djProfiles');
 
   var featuredPost = Posts.findOne({ approved: true, featured: true },
     { sort: { submitted: -1 } });
 
   return {
-    ready: s1.ready() && s2.ready() && s3.ready(),
+    ready: s1.ready() && s2.ready() && s3.ready() && s4.ready(),
     pages: pages(Posts.find((s2.ready() && { _id:
        { $ne: featuredPost._id }, approved: true } || null) ||
        (!s2.ready && { approved: true } || null),
