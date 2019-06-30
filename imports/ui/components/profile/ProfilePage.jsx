549e0efc3171632f76dc4bc2eca46a96128440d5
dchan3 <dchan3@hawaii.edu>
2019-06-29 19:11:29 -1000

    Fix Profile Page bug

--- imports/ui/components/profile/ProfilePage.jsx
+++ imports/ui/components/profile/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { object, array } from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { FlowRouter } from 'meteor/kadira:flow-router';
@@ -9,116 +9,92 @@ import { Bert } from 'meteor/themeteorchef:bert';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Metamorph } from 'react-metamorph';
 
+function ProfileSocialLink(url, handle, icon) {
+  if (handle) return <a href={`${url}${handle}`}>
+    <img className='profile__social-icon' src={`/img/${icon}.png`} /></a>
+  else return null;
+}
+
 function ProfilePage({
   profile, show, posts
 }) {
-  function handleBan() {
+  function toggleBan() {
     if (Meteor.user().hasRole('admin')) {
-      var username = FlowRouter.getParam('username');
-      var user = Meteor.users.findOne({ username: username });
-      var profile = Profiles.findOne({ userId: user._id });
-      Profiles.update(profile._id, { $set: { banned: true } });
-      Bert.alert(`User @${username} banned.`, 'default');
+      let username = FlowRouter.getParam('username'),
+        user = Meteor.users.findOne({ username }),
+        profile = Profiles.findOne({ userId: user._id });
+      Profiles.update(profile._id, { $set: { banned: !profile.banned } });
+      Bert.alert(
+        `User @${username} ${profile.banned ? 'un' : ''}banned.`, 'default');
     }
   }
 
-  function handleUnban() {
-    if (Meteor.user().hasRole('admin')) {
-      var username = FlowRouter.getParam('username');
-      var user = Meteor.users.findOne({ username: username });
-      var profile = Profiles.findOne({ userId: user._id });
-      Profiles.update(profile._id, { $set: { banned: false } });
-      Bert.alert(`User @${username}'s ban lifted.`, 'default');
-    }
-  }
+  useEffect(() => {}, [Meteor.user() && Meteor.user().hasRole('admin')]);
 
   if (profile !== undefined && !profile.banned || Meteor.user() !== null &&
     Meteor.user().hasRole('admin')) {
-    return [
-      <Metamorph title={`${profile.name
-      }'s Profile - KTUH FM Honolulu | Radio for the People`}
-      description={`${profile.name}'s Profile`}
-      image={profile.photo && profile.photo.url ||
-        'https://ktuh.org/img/ktuh-logo.jpg'} />,
-      <h2 className='general__header'>{profile.name}</h2>,
-      <div className='profile'>
-        {Meteor.userId() && profile.userId === Meteor.userId() && (
-          <div>
-            <a href='/profile'>
-              <button type='button'
-                className='btn btn-default btn-md party-header__button'>
-                <span className='glyphicon glyphicon-edit'
-                  aria-hidden='true'></span>
-                Edit
-              </button>
-            </a>
+    let { name, photo, userId, thumbnail, website, twitter, facebook,
+      snapchat, instagram, soundcloud } = profile;
+    return [<Metamorph title={`${name
+    }'s Profile - KTUH FM Honolulu | Radio for the People`}
+    description={`${name}'s Profile`} image={photo && photo.url ||
+      'https://ktuh.org/img/ktuh-logo.jpg'} />,
+    <h2 className='general__header'>{name}</h2>,
+    <div className='profile'>
+      {Meteor.userId() && userId === Meteor.userId() && (
+        <div>
+          <a href='/profile'>
+            <button type='button'
+              className='btn btn-default btn-md party-header__button'>
+              <span className='glyphicon glyphicon-edit'
+                aria-hidden='true'></span>
+              Edit
+            </button>
+          </a>
+        </div>) || null}
+      <div className='profile__left'>
+        <img className='profile__pic'
+          src={((thumbnail || null) ||
+          (photo && photo.url || null)) ||
+          ((!thumbnail && !photo) && 'https://ktuh.org/img/ktuh-logo.jpg' ||
+          null)} />
+        {(website || twitter || facebook || snapchat || soundcloud || instagram)
+          && (<div className='profile__social-icons'>
+            {[
+              ['', website, 'website'],
+              ['http://soundcloud.com/', soundcloud, 'soundcloud'],
+              ['http://instagram.com/', instagram, 'instagram'],
+              ['http://facebook.com/', facebook, 'facebook'],
+              ['http://twitter.com/', twitter, 'twitter'],
+              ['http://snapchat.com/add/', snapchat, 'snapchat']
+            ].map((params) => ProfileSocialLink(...params))}
           </div>) || null}
-        <div className='profile__left'>
-          <img className='profile__pic'
-            src={((profile.thumbnail || null) ||
-            (profile.photo && profile.photo.url || null)) ||
-            ((!profile.thumbnail && !profile.photo) &&
-            'https://ktuh.org/img/ktuh-logo.jpg' ||
-            null)} />
-          {(profile.website || profile.twitter || profile.facebook ||
-            profile.snapchat || profile.soundcloud || profile.instagram) &&
-            (<div className='profile__social-icons'>
-              {profile.website &&
-                <a href={profile.website}>
-                  <img className='profile__social-icon'
-                    src='/img/website.png' /></a> || null}
-              {profile.soundcloud &&
-                <a href=
-                  {`http://soundcloud.com/${profile.soundcloud}`}>
-                  <img className='profile__social-icon'
-                    src='/img/soundcloud.png' /></a> || null}
-              {profile.instagram &&
-                <a href=
-                  {`http://instagram.com/${profile.instagram}`}>
-                  <img className='profile__social-icon'
-                    src='/img/instagram.png' /></a> || null}
-              {profile.facebook &&
-              <a href={`http://facebook.com/${profile.facebook}`}>
-                <img className='profile__social-icon'
-                  src='/img/facebook.png' /></a> || null}
-              {profile.twitter &&
-                <a href={`http://twitter.com/${profile.twitter}`}>
-                  <img className='profile__social-icon'
-                    src='/img/twitter.png' /></a> || null}
-              {profile.snapchat &&   <a href=
-                {`https://www.snapchat.com/add/${profile.snapchat}`}>
-                <img className='profile__social-icon'
-                  src='/img/snapchat.png' /></a> || null}
-            </div>) || null}
-          {!!show &&
-            <div className='profile__show-link'>
-              <a className='color-button white-button profile__show-button'
-                href={`/shows/${show.slug}`}>View Show Page</a>
-            </div> || null}
-        </div>
-        <div className='profile__info'>
-          <div className='profile__bio'
-            dangerouslySetInnerHTML={{ __html: profile.bio ||
-            `<i>(${profile.name} hasn't filled out a bio yet.)</i>` }} />
-          {posts && posts.length &&
-          <div className='profile__posts'>
-            <h4>Posts</h4>
-            {posts.map((post) =>
-              <p key={post._id} className='profile__posts'>
-                <a href={`/radioblog/${post.slug}`}>{post.title}</a>
-              </p>)}
+        {!!show &&
+          <div className='profile__show-link'>
+            <a className='color-button white-button profile__show-button'
+              href={`/shows/${show.slug}`}>View Show Page</a>
           </div> || null}
-          {((Meteor.userId() && profile.userId === Meteor.userId()) ||
-            (Meteor.user() && Meteor.user().hasRole('admin')) &&
-            profile.userId === Meteor.userId()) &&
-            (!profile.banned &&
-            <input id='profile__ban-user' type="button" value="Ban User"
-              onClick={handleBan} /> ||
-            <input id='profile__unban-user' type="button"
-              value="Lift User Ban" onClick={handleUnban} />) || null}
-        </div>
       </div>
-    ];
+      <div className='profile__info'>
+        <div className='profile__bio'
+          dangerouslySetInnerHTML={{ __html: profile.bio ||
+          `<i>(${profile.name} hasn't filled out a bio yet.)</i>` }} />
+        {posts && posts.length &&
+        <div className='profile__posts'>
+          <h4>Posts</h4>
+          {posts.map((post) =>
+            <p key={post._id} className='profile__posts'>
+              <a href={`/radioblog/${post.slug}`}>{post.title}</a>
+            </p>)}
+        </div> || null}
+        {(Meteor.user() && Meteor.user().hasRole('admin') &&
+          profile.userId !== Meteor.userId()) && (!profile.banned &&
+          <input id='profile__ban-user' type="button" value="Ban User"
+            onClick={toggleBan} /> ||
+          <input id='profile__unban-user' type="button"
+            value="Lift User Ban" onClick={toggleBan} />) || null}
+      </div>
+    </div>];
   }
   else {
     return <p>This user does not have a profile.</p>;
@@ -129,7 +105,7 @@ ProfilePage.propTypes = {
   profile: object,
   show: object,
   posts: array
-}
+};
 
 export default withTracker(() => {
   var username = FlowRouter.getParam('username')
@@ -147,9 +123,9 @@ export default withTracker(() => {
 
   return {
     profile: (function() {
-      var username = FlowRouter.getParam('username');
-      var user = Meteor.users.findOne({ username: username });
-      var profile = user && Profiles.findOne({ userId: user._id });
+      var username = FlowRouter.getParam('username'),
+        user = Meteor.users.findOne({ username }),
+        profile = user && Profiles.findOne({ userId: user._id });
 
       if (profile !== undefined) {
         return profile;
